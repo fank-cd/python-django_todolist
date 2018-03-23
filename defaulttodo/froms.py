beba1fde696a4453905fd3989dcdb4929e9cd05c
daye <2464512446@qq.com>
2018-03-23 17:30:34 +0800

    form dev

--- defaulttodo/froms.py
+++ defaulttodo/froms.py
@@ -1,3 +1,12 @@
+#coding:utf-8
 from django import forms
+from .models import Item
 
+class UserForm(forms.ModelForm):
+    name = forms.CharField(max_length=120,help_text='输入名字啊傻逼')
+    password =forms.CharField(max_length=120,widget=forms.PasswordInput)
 
+class ItemForm(forms.ModelForm):
+    class Mata:
+        model = Item
+        fields = ('item_name','item_description','priority')
--- defaulttodo/views.py
+++ defaulttodo/views.py
@@ -3,7 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from .froms import UserForm
+from .models import Item,User
 # Create your views here.
 
 def index(request):
-    return HttpResponse('ddd')
+    return render(request,'default/index.html')
+
+
+def login(request):
+    return HttpResponse('dddddd')
+
+def resgister(request):
+    if request.method == "POST":
+        user_form =UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.pass
